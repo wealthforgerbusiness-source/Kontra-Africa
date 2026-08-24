fba43307df83c6191494551828f39122e6940955
wealthforgerbusiness-source <wealthforgerbusiness@gmail.com>
2026-08-24 21:11:25 +0100

    Update config.js

--- functions/src/config.js
+++ functions/src/config.js
@@ -6,7 +6,8 @@ const admin = require("firebase-admin");
 // L'initialisation utilise automatiquement la variable d'environnement GOOGLE_APPLICATION_CREDENTIALS
 // Assurez-vous d'avoir téléchargé serviceAccountKey.json depuis les paramètres du projet Firebase
 admin.initializeApp({
-  credential: admin.credential.applicationDefault()
+  credential: admin.credential.applicationDefault(),
+  storageBucket: "kontra-africa.firebasestorage.app"
 });
 
 const db = admin.firestore();
